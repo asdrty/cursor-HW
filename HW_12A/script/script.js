a1d51e7421bee69ff4a7d033f6c8de9bca85ce20
Nasia <nastya16081998@gmail.com>
2022-06-26 00:53:50 +0300

    add Event click

--- HW_12A/script/script.js
+++ HW_12A/script/script.js
@@ -3,22 +3,43 @@ let arrId =[];
 for (let index = 0; index < mainDivAudio.length; index++) {
     arrId.push(mainDivAudio[index].id)
 }
+const divClick = document.querySelector('.mainDiv');
 const divAudio = document.querySelectorAll(".blockLetter");
+let arrIdDiv = [];
+for (let index = 0; index < divAudio.length; index++) {
+    arrIdDiv.push(divAudio[index].id)
+}
 
-window.addEventListener("keydown", function(event) {
-    divAudio.forEach((el) => el.classList.remove("playing"))
-    mainDivAudio.forEach((el) => el.pause())
-    const key = event.key.toLocaleLowerCase(); 
-    let keyCh = arrId.find((elem) => elem === key);
-    if(!keyCh) return console.log("I don't know what are sound")
-    let audio = document.getElementById(keyCh);
-    let divAudioLetter = document.getElementById(keyCh.toLocaleUpperCase());
+function playAudio (key) {
+    let audio = document.getElementById(key);
+    let divAudioLetter = document.getElementById(key.toLocaleUpperCase());
     divAudioLetter.classList.add("playing");
     audio.currentTime = 0;
     audio.play();
     audio.addEventListener('ended', () => {
         divAudioLetter.classList.remove('playing');
      })
+}
+
+function removePlay() {
+    divAudio.forEach((el) => el.classList.remove("playing"))
+    mainDivAudio.forEach((el) => el.pause())
+}
+
+function divClicks(elem) {
+    removePlay()
+    const divClickLetter = elem.innerHTML.toLocaleLowerCase();
+    playAudio(divClickLetter);
+}
+
+divClick.addEventListener("click", (el) => divClicks(el.target))
+
+window.addEventListener("keydown", function(event) {
+    removePlay()
+    const key = event.key.toLocaleLowerCase(); 
+    let keyCh = arrId.find((elem) => elem === key);
+    if(!keyCh) return console.log("I don't know what are sound");
+    playAudio(keyCh);
   }, true);
 
 
